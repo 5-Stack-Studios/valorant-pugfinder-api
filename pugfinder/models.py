25b05f82eeacc5c977db6684f8ac273229c661eb
Josh Sanchez <joshuaesanch@gmail.com>
2020-06-03 00:04:06 -0500

    Add Match model; Create relationship from Users to Match Closes #2

--- pugfinder/models.py
+++ pugfinder/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from pugfinder.database import Base
 
 class User(Base):
@@ -8,6 +8,7 @@ class User(Base):
     email = Column(String(120), unique=True)
     hashed_pass = Column(String(128))
     preferred_role = Column(String(10))
+    curr_match_id = Column(Integer, ForeignKey('matches'))
 
     def __init__(self, username=None, email=None, hashed_pass=None, preferred_role=None):
         self.username = username
@@ -17,3 +18,20 @@ class User(Base):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+class Match(Base):
+    __tablename__ = 'matches'
+    id = Column(Integer, index=True, primary_key=True)
+    is_active = Column(Boolean)
+    is_public = Column(Boolean)
+    map = Column(String(20))
+    players = relationship('users')
+
+    def __init__(self, is_active=None, is_public=None, map=None):
+        self.is_active = is_active
+        self.is_public = is_public
+        self.map = map
+        self.players = []
+    
+    def __repr__(self):
+        return '<Match %r>' % self.map
