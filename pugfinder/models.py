76ed455c55a34ce31b428bf6d8039a453afa2b1d
Josh Sanchez <joshuaesanch@gmail.com>
2020-06-05 22:19:33 -0500

    Fix Model ForeignKey relationship issue

--- pugfinder/models.py
+++ pugfinder/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from pugfinder.database import Base
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -8,9 +10,10 @@ class User(Base):
     email = Column(String(120), unique=True)
     hashed_pass = Column(String(128))
     preferred_role = Column(String(10))
-    curr_match_id = Column(Integer, ForeignKey('matches'))
+    curr_match_id = Column(Integer, ForeignKey('matches.id'))
 
-    def __init__(self, username=None, email=None, hashed_pass=None, preferred_role=None):
+    def __init__(self, username=None, email=None,
+                 hashed_pass=None, preferred_role=None):
         self.username = username
         self.email = email
         self.hashed_pass = hashed_pass
@@ -19,19 +22,19 @@ class User(Base):
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+
 class Match(Base):
     __tablename__ = 'matches'
     id = Column(Integer, index=True, primary_key=True)
     is_active = Column(Boolean)
     is_public = Column(Boolean)
     map = Column(String(20))
-    players = relationship('users')
+    players = relationship('User', backref='player')
 
     def __init__(self, is_active=None, is_public=None, map=None):
         self.is_active = is_active
         self.is_public = is_public
         self.map = map
-        self.players = []
-    
+
     def __repr__(self):
         return '<Match %r>' % self.map
