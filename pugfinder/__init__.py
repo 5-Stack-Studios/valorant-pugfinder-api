92eb6df8601ebe324ba99290994f0bf5be9e873a
Patrick Gallagher <pjg4@illinois.edu>
2020-06-16 00:15:47 -0500

    add socketio endpoints; add todos for user routes

--- pugfinder/__init__.py
+++ pugfinder/__init__.py
@@ -1,40 +1,83 @@
 import os
 
-from flask import Flask
-from flask_socketio import SocketIO
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
 
 from pugfinder.database import init_db
+from pugfinder.models import User
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    socketio = SocketIO(app)
-    socketio.run(app)
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
+init_db()
+
+# a simple page that says hello
+@app.route('/api')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if request.method == 'POST':
+        data = request.get_json()
+        required_params = set("username", "email", "password")
+        if len(set(data.keys()) & required_params) != len(required_params):
+            user = User(username=data['username'],
+                        hashed_pass="", # TODO: Bcrypt
+                        email=data['email'])
+        # TODO: Construct JWT and send back
+    elif request.method == 'GET':
+        # TODO: Return list of users
         pass
 
-    init_db()
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
 
-    return app
+# Temporary measure; should eventually be replaced with
+# a value from the config
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+cnt = 0
+
+@socketio.on('lobby.create')
+def create_lobby(data):
+    global cnt
+    cnt += 1
+    emit('lobby.create.success', {
+        'error': False,
+        'payload': {
+            'id': cnt,
+            'owner': data['owner'],
+            'map': data['map'],
+            'team1': [],
+            'team2': [],
+            'queue': {
+                
+            }
+        }
+    }, broadcast=True)
+
+@socketio.on('message')
+def mesg(message):
+    print(message)
+
+@socketio.on('connect')
+def on_connect():
+    print('connected')
